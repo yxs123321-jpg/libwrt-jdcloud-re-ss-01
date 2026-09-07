65565aee8b31b68995c56a1075053a20ebcc2ba6
as <as@asdeiMac.local>
2026-09-07 17:39:08 +0800

    修改daed

--- diy.sh
+++ diy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # ==============================================
 # LibWrt 定制脚本 - 京东云亚瑟 RE-SS-01
-# 1. 默认后台地址改为 10.0.0.1
-# 2. 默认 root 密码改为 root
+# 1. 默认后台地址改为 192.168.50.1
+# 2. 默认 root 密码改为指定哈希
 # 3. 默认主题改为 Argon
 # ==============================================
 set -e
 
-# 1. 修改默认 LAN IP（192.168.1.1 -> 10.0.0.1）
-sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+# 1. 修改默认 LAN IP（192.168.1.1 -> 192.168.50.1）
+sed -i 's/192\.168\.1\.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修改默认 root 密码为 root
-# sed -i 's|^root:::|root:$1$wbroot$wOBLV0MeQc.zQXkjnygcq.:|' package/base-files/files/etc/shadow
+# 2. 修改默认 root 密码
+sed -i 's|^root:[^:]*:|root:$5$FEvKqB47IGg0kMEb$0L5AE92U9CgakKhWSKdBuK/vAzL/GaOqmEkR1oxUbd5:|' package/base-files/files/etc/shadow
 
 # 3. 设置默认主题为 Argon
 mkdir -p files/etc/uci-defaults
